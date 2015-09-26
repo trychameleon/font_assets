14948fac850cbab74a7e880a8d18aea88a2432f2
Brian Norton <brian.nort@gmail.com>
2015-09-25 17:19:07 -0700

    Use the Rails 4 way of adding middleware.

--- lib/font_assets/railtie.rb
+++ lib/font_assets/railtie.rb
@@ -8,13 +8,7 @@ module FontAssets
       config.font_assets.origin ||= '*'
       config.font_assets.options ||= { allow_ssl: true }
 
-      insert_target = if defined?(ActionDispatch::Static)
-        'ActionDispatch::Static'
-      else
-        'Rack::Runtime'
-      end
-
-      app.middleware.insert_before insert_target, FontAssets::Middleware, config.font_assets.origin, config.font_assets.options
+      app.middleware.use FontAssets::Middleware, config.font_assets.origin, config.font_assets.options
     end
   end
 end
